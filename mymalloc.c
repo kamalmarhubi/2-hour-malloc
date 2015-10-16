105e552ff3c459a0b3f3279d59473ec3d8b394fa
Kamal Marhubi <kamal@marhubi.com>
2015-10-16 13:39:35 -0400

    Add some utilities to help with sbrk

--- mymalloc.c
+++ mymalloc.c
@@ -1,5 +1,7 @@
 #include "mymalloc.h"
 #include <stddef.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 
 struct metadata {
@@ -12,14 +14,62 @@ struct metadata {
 struct allocation {
     size_t size;
     struct allocation* next;  // only if free
-    char data[1];
+    char data[1];  // the data given to the caller
 };
 
 
 static struct metadata md = {
     .next_sbrk = 1024,
+    .next_alloc = NULL,
     .free_list = NULL,
 };
+
+
+size_t remaining_space() {
+    char* alloc_loc = md.next_alloc;
+    char* curr_brk = sbrk(0);
+
+    return curr_brk - alloc_loc;
+}
+
+
+size_t required(size_t size) {
+    return size + sizeof(struct allocation);
+}
+
+
+// Check if an allocation of size can be made without moving program break. This
+// check includes the size of the allocation struct.
+bool fits_without_sbrk(size_t size) {
+    return remaining_space() > required(size);
+}
+
+
+bool fits_with_sbrk(size_t size) {
+    char* curr_brk = sbrk(0);
+    char* alloc_loc = md.next_alloc;
+    return (curr_brk + md.next_sbrk - alloc_loc) > size;
+}
+
+
+// Ensures that an allocation of size is possible, extending program data space
+// if necessary.
+int ensure_fits(size_t size) {
+    if (fits_without_sbrk(size)) {
+        return;
+    }
+
+    size_t next_sbrk = md.next_sbrk;
+
+    while(!fits_with_sbrk(size)) {
+        md.next_sbrk *= 2;
+    }
+
+    // TODO: error handling
+    sbrk(md.next_sbrk);
+
+    return 0;
+}
 
 
 // Returns the pointer to struct allocation that ptr is the data member of.
@@ -34,6 +84,9 @@ struct allocation* alloc_of(void* ptr) {
 
 
 void *mymalloc(size_t size) {
+    ensure_fits(size);
+
+
     return NULL;
 }
 
