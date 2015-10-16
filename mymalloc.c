8d445755f311974a7225ee6f536b3b3428550c9a
Kamal Marhubi <kamal@marhubi.com>
2015-10-16 13:02:59 -0400

    Add global metadata

--- mymalloc.c
+++ mymalloc.c
@@ -1,9 +1,24 @@
 #include "mymalloc.h"
 #include <stddef.h>
 
+
+struct metadata {
+    size_t next_sbrk;  // next amount sbrk by
+    void* next_alloc;  // next allocation if can't be filled from free list
+    struct allocation *free_list;
+};
+
+
 struct allocation {
     size_t size;
     struct allocation* next;  // only if free
+    char data[1];
+};
+
+
+static struct metadata md = {
+    .next_sbrk = 1024,
+    .free_list = NULL,
 };
 
 
